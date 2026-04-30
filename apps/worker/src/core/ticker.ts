593b990a0bb48910eed911048ac48ac4ac24ad29
Natalie512 <congdonnathan@gmail.com>
2026-04-30 16:10:35 -0500

    THIS one works

--- apps/worker/src/core/ticker.ts
+++ apps/worker/src/core/ticker.ts
@@ -3,12 +3,14 @@ import cron from 'node-cron';
 
 // Run a task every hour
 cron.schedule('0 * * * *', () => {
-  console.log('Task is running every hour');
+  hourlyTask();
 });
 
 // Run a task every second
-cron.schedule('0 * * * * *', () => {
+cron.schedule('* * * * * *', () => {
   console.log('Task is running every second');
 });
 
-
+function hourlyTask() {
+  console.log('This task runs every hour');
+}
